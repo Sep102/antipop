a5e42d4777e7fc4f9bdf8592563eb4d5d1cb3825
Michael Lapinsky <mlapinsk@andesitestudios.com>
2010-10-27 01:28:56 -0700

    Added antipopd source files
    - Simple purely does "say ' '" every ten seconds no matter the power
      profile
    - antipopd-with-pmset checks for an ac_only file on startup and uses its
      contents to determine if it should check the "pmset -g" output every
      ten seconds to only "say" when not on battery

---
+++ antipopd-simple.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+int main()
+{
+    while(1)
+    {
+	system("say ' '");
+	sleep(10);
+    }
+}
---
+++ antipopd-with-pmset.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int ac_only = 0;
+
+    FILE *ac_file = fopen("/usr/local/share/antipop/ac_only", "r");
+    if(ac_file)
+    {
+	/* Should be a single integer in the file:
+	 * 0 = AC or Battery
+	 * >0 = AC only
+	 */
+	ac_only = fgetc(ac_file);
+	fclose(ac_file);
+    }
+
+    if(!ac_only)
+    {
+	while(1)
+	{
+	    system("say ' '");
+	    sleep(10);
+	}
+    }
+    else
+    {
+	while(1)
+	{
+	    FILE *ac_check_proc = popen("pmset -g | grep \"AC Power.*\\*\"", "r");
+	    if(ac_check_proc && !feof(ac_check_proc))
+	    {
+		system("say ' '");
+		pclose(ac_check_proc);
+	    }
+
+	    sleep(10);
+	}
+    }
+}
